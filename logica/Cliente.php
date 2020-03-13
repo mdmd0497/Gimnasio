8a4b38436647085681ed4ba6ad0ed710777591a1
saf <fas>
2020-03-12 21:46:36 -0500

    se crean clases de cliente

---
+++ logica/Cliente.php
@@ -0,0 +1,55 @@
+<?php
+require 'persistencia/ClienteDAO.php';
+require_once 'persistencia/Conexion.php';
+
+class Cliente extends Persona
+{
+    private $observaciones;
+    private $estado;
+    private $telefono;
+    private $pacienteDAO;
+    private $conexion;
+    private $foto;
+
+    
+    
+    function getEstado()
+    {
+        return $this->estado;
+    }
+    function getobservaciones()
+    {
+        return $this->observaciones;
+    }
+    function getTelefono()
+    {
+        return $this->telefono;
+    }
+    
+    function getFoto()
+    {
+        return $this->foto;
+    }
+    
+
+    
+    
+    function Paciente($id = "", $nombre = "", $apellido = "", $correo = "",$salt, $clave = "",$foto= "",$telefono= "",$observaciones= "",$estado= "")
+    {
+        $this->Persona($id, $nombre, $apellido, $correo,$salt, $clave);
+        $this->foto = $foto;
+        $this->telefono = $telefono;
+        $this->observaciones = $observaciones;
+        $this->estado = $estado;
+
+        $this->conexion = new Conexion();
+        $this->pacienteDAO = new ClienteDAO($id, $nombre, $apellido, $correo,$salt, $clave, $foto, $telefono, $observaciones, $estado);
+    }
+    
+    function registrar()
+    {
+        $this->conexion->abrir();
+        $this->conexion->ejecutar($this->ClienteDAO->registrar());
+        $this->conexion->cerrar();
+    }
+    
