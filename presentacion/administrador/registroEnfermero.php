57d4509db21665001c56281aba845a0f610f3a88
Oscar Cely <oscarcej@gmail.com>
2020-03-19 11:26:16 -0500

    Se hace merge con ramaMichael

--- presentacion/administrador/registroEnfermero.php
+++ presentacion/administrador/registroEnfermero.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_SESSION["id"])){
+if (!isset($_SESSION["id"])) {
     header("Location: index.php");
     exit();
 }
@@ -14,19 +14,19 @@ $administrador->consultar();
     ?>
 </header>
 <?php
-$error = - 1;
+$error = -1;
 $nombre = "";
 $apellido = "";
 $correo = "";
 $clave = "";
 $telefono = "";
 if (isset($_POST["registrar_enfermero"])) {
-    
+
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $correo = $_POST["correo"];
     $telefono = $_POST["telefono"];
-    
+
     $enfermero = new Enfermero("", "", "", $correo);
     if (!$enfermero->existeCorreo()) {
         $password = $_POST["clave"];
@@ -42,55 +42,58 @@ if (isset($_POST["registrar_enfermero"])) {
 
 ?>
 <div class="container-fluid" id="reg-enfermero">
-	<div class="row justify-content-center">
-		<div class="col col-md-auto col-lg-9">
-			<div class="card">
-				<div class="card-header bg-primary text-white">Registro Enfermero</div>
-				<div class="card-body">
+    <div class="row justify-content-center">
+        <div class="col col-md-auto col-lg-9">
+            <div class="card">
+                <div class="card-header bg-primary text-white">Registro Enfermero</div>
+                <div class="card-body">
                     <?php
                     if ($error == 0) {
                         ?>
                         <div class="alert alert-success" role="alert">
-						enfermero registrado exitosamente.</div>
+                            enfermero registrado exitosamente.
+                        </div>
                     <?php } else if ($error == 1) { ?>
                         <div class="alert alert-danger" role="alert">
                             El correo <?php echo $correo; ?> ya existe
                         </div>
                     <?php } ?>
                     <form
-						action="<?php echo "index.php?pid=" . base64_encode("presentacion/administrador/registroEnfermero.php") . "&action=signup" ?>"
-						method="post">
-						<div class="form-group">
-							<input type="text" name="nombre" class="form-control"
-								placeholder="Nombre" required="required"
-								value="<?php echo $nombre; ?>">
-						</div>
-						<div class="form-group">
-							<input type="text" name="apellido" class="form-control"
-								placeholder="Apellido" required="required"
-								value="<?php echo $apellido; ?>">
-						</div>
-						<div class="form-group">
-							<input type="email" name="correo" class="form-control"
-								placeholder="Correo" required="required"
-								value="<?php echo $correo; ?>">
-						</div>
-						<div class="form-group">
-							<input type="password" name="clave" class="form-control"
-								placeholder="Clave" required="required">
-						</div>
-						<div class="form-group">
-							<input type="text" name="telefono" class="form-control"
-								placeholder="telefono" required="required"
-								value="<?php echo $telefono; ?>">
-						</div>
-						<button type="submit" name="registrar_enfermero" class="btn btn-primary">Registrar</button>
-                        <a class="btn btn-secondary" href="index.php" style="float: right">Volver</a>
-					</form>
-				</div>
-			</div>
-		</div>
+                            action="<?php echo "index.php?pid=" . base64_encode("presentacion/administrador/registroEnfermero.php") . "&action=signup" ?>"
+                            method="post">
+                        <div class="form-group">
+                            <input type="text" name="nombre" class="form-control"
+                                   placeholder="Nombre" required="required"
+                                   value="<?php echo $nombre; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="apellido" class="form-control"
+                                   placeholder="Apellido" required="required"
+                                   value="<?php echo $apellido; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="correo" class="form-control"
+                                   placeholder="Correo" required="required"
+                                   value="<?php echo $correo; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="clave" class="form-control"
+                                   placeholder="Clave" required="required">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="telefono" class="form-control"
+                                   placeholder="telefono" required="required"
+                                   value="<?php echo $telefono; ?>">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="registrar_enfermero" class="btn btn-primary">Registrar</button>
+                            <a class="btn btn-secondary" href="index.php" style="float: right">Volver</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-	</div>
+    </div>
 
 </div>
--- presentacion/administrador/registroEntrenador.php
+++ presentacion/administrador/registroEntrenador.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_SESSION["id"])){
+if (!isset($_SESSION["id"])) {
     header("Location: index.php");
     exit();
 }
@@ -14,23 +14,23 @@ $administrador->consultar();
     ?>
 </header>
 <?php
-$error = - 1;
+$error = -1;
 $nombre = "";
 $apellido = "";
 $correo = "";
 $telefono = "";
 if (isset($_POST["registrar_entrenador"])) {
-    
+
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $correo = $_POST["correo"];
     $telefono = $_POST["telefono"];
-    
-    $entrenador = new Entrenador("","", "", $correo);
-    
+
+    $entrenador = new Entrenador("", "", "", $correo);
+
     if (!$entrenador->existeCorreo()) {
         $password = $_POST["clave"];
-        $entrenador = new Entrenador("", $nombre, $apellido, $correo, password_hash($password, PASSWORD_BCRYPT), "", $telefono,"");
+        $entrenador = new Entrenador("", $nombre, $apellido, $correo, password_hash($password, PASSWORD_BCRYPT), "", $telefono, "");
         $entrenador->registrar();
         $error = 0;
     } else {
@@ -41,56 +41,58 @@ if (isset($_POST["registrar_entrenador"])) {
 
 
 ?>
-<div class="container-fluid" style="margin-top: 20px;">
-	<div class="row">
-		<div class="col-3"></div>
-		<div class="col-6">
-			<div class="card">
-				<div class="card-header bg-primary text-white">Registro Entrenador</div>
-				<div class="card-body">
+<div class="container-fluid" id="reg-enfermero">
+    <div class="row justify-content-center">
+        <div class="col col-md-auto col-lg-9">
+            <div class="card">
+                <div class="card-header bg-primary text-white">Registro Entrenador</div>
+                <div class="card-body">
                     <?php
                     if ($error == 0) {
                         ?>
                         <div class="alert alert-success" role="alert">
-						entrenador registrado exitosamente.</div>
+                            entrenador registrado exitosamente.
+                        </div>
                     <?php } else if ($error == 1) { ?>
                         <div class="alert alert-danger" role="alert">
                             El correo <?php echo $correo; ?> ya existe
                         </div>
                     <?php } ?>
                     <form
-						action="<?php echo "index.php?pid=" . base64_encode("presentacion/administrador/registroEntrenador.php") . "&action=signup" ?>"
-						method="post">
-						<div class="form-group">
-							<input type="text" name="nombre" class="form-control"
-								placeholder="Nombre" required="required"
-								value="<?php echo $nombre; ?>">
-						</div>
-						<div class="form-group">
-							<input type="text" name="apellido" class="form-control"
-								placeholder="Apellido" required="required"
-								value="<?php echo $apellido; ?>">
-						</div>
-						<div class="form-group">
-							<input type="email" name="correo" class="form-control"
-								placeholder="Correo" required="required"
-								value="<?php echo $correo; ?>">
-						</div>
-						<div class="form-group">
-							<input type="password" name="clave" class="form-control"
-								placeholder="Clave" required="required">
-						</div>
-						<div class="form-group">
-							<input type="text" name="telefono" class="form-control"
-								placeholder="telefono" required="required"
-								value="<?php echo $telefono; ?>">
-						</div>
-						<button type="submit" name="registrar_entrenador" class="btn btn-primary">Registrar</button>
-					</form>
-				</div>
-			</div>
-		</div>
+                            action="<?php echo "index.php?pid=" . base64_encode("presentacion/administrador/registroEntrenador.php") . "&action=signup" ?>"
+                            method="post">
+                        <div class="form-group">
+                            <input type="text" name="nombre" class="form-control"
+                                   placeholder="Nombre" required="required"
+                                   value="<?php echo $nombre; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="apellido" class="form-control"
+                                   placeholder="Apellido" required="required"
+                                   value="<?php echo $apellido; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="correo" class="form-control"
+                                   placeholder="Correo" required="required"
+                                   value="<?php echo $correo; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="clave" class="form-control"
+                                   placeholder="Clave" required="required">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="telefono" class="form-control"
+                                   placeholder="telefono" required="required"
+                                   value="<?php echo $telefono; ?>">
+                        </div>
+  
+                        <div class="form-group">
+                            <button type="submit" name="registrar_enfermero" class="btn btn-primary">Registrar</button>
+                            <a class="btn btn-secondary" href="index.php" style="float: right">Volver</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-	</div>
-
-</div>
+    </div>
